bc9377e340ce5b39e64f0c42fcd6d162aa8f1030
jimmy-s <jimmy.seletsoane@reverside.co.za>
2015-03-09 14:09:12 +0200

    Added authentication and Authorization using a database

--- recruitment-api/src/main/resources/data.sql
+++ recruitment-api/src/main/resources/data.sql
@@ -1,3 +1,13 @@
 insert into resource (name, description) values('task-1', 'task-one');
 insert into resource (name, description) values('task-2', 'task-two');
 insert into resource (name, description) values('task-3', 'task-three');
+
+
+
+INSERT INTO  users (username,password,enabled) VALUES ('Pradyumna','12345',TRUE );
+INSERT INTO  users (username,password,enabled) VALUES ('Jamie','23456',TRUE );
+INSERT INTO  users (username,password,enabled) VALUES ('ManMay','34567',TRUE );
+
+INSERT INTO   user_roles (username,role) VALUES ('Pradyumna','ROLE_CLIENT');
+INSERT INTO   user_roles (username,role) VALUES ('Jamie','ROLE_CANDIDATE');
+INSERT INTO   user_roles (username,role) VALUES ('Manmay','ROLE_ADMIN');
